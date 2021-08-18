cab8a28e216a83daff6384d37afadb0ef3637985
Dmytro Petrenko <dmg.petrenko@gmail.com>
2021-08-18 18:02:51 +0300

    core - added winston logger

--- src/core/constants/app.constants.ts
+++ src/core/constants/app.constants.ts
@@ -2,3 +2,4 @@ import * as packageJson from '../../../package.json';
 
 export const APP_ROOT = `/api/v${packageJson.version}`;
 export const APP_ROOT_MESSAGE = 'Root Endpoint Healthcare Courses API';
+export const DEFAULT_DATE_FORMAT = 'DD-MM-YYYY';
--- src/core/router/route-wrapper.ts
+++ src/core/router/route-wrapper.ts
@@ -1,11 +1,16 @@
 import { NextFunction, Request } from 'express';
 import { RequestHandler, Response } from 'express-serve-static-core';
+import { logger } from '../utils';
 
 export function wrapRouteAction(action: RequestHandler) {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
+      logger.info(
+        `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
+      );
       await action(req, res, next);
     } catch (e) {
+      logger.error('', e);
       return next(e);
     }
   };
